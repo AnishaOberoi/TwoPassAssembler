bdccd79c7cfd1dfa9ce3ba522b08d1a74be87b63
AnishaOberoi <74367388+AnishaOberoi@users.noreply.github.com>
2022-06-30 10:54:30 +0530

    code update

--- main.c
+++ main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
